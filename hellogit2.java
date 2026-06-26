4c07e4d18f16371136bfe21070c7a562cb3549d0
Alejandro Magno <Magno.sayhua2007@gmail.com>
2026-06-26 18:38:45 -0500

    Mi sexto commit

--- hellogit2.java
+++ hellogit2.java
@@ -1,5 +1,5 @@
 public class hellogit2 {
     public static void main(String[] args) {
-        System.out.println("Hello Git2!");
+        System.out.println("Hello Git 2!");
     }
 }
---
+++ hellogit3.java
@@ -0,0 +1,5 @@
+public class main hellogit3 {
+    public static void main (String [] args) {
+        System.out.println("Hello Git 3!");
+    }
+}
